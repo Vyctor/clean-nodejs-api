9887ecc3dd535937519843dfae000d7a48728faf
Vyctor <dev.vyctor@gmail.com>
2021-09-21 19:12:39 -0300

    test: ensure SignUp controller returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -45,4 +45,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: password'))
   })
+
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'vyctor',
+        email: 'vyctorguimaraes@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: password_confirmation'))
+  })
 })
