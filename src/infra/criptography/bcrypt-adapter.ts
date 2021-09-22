49fd38992fa52d2d047aecd2493c47fa975077ad
Vyctor <dev.vyctor@gmail.com>
2021-09-22 20:11:38 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,17 @@
+
+import bcryptjs from 'bcryptjs'
+
+import { Encrypter } from '../../data/protocols/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await bcryptjs.hash(value, this.salt)
+    return null
+  }
+}
