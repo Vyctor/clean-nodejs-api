0a5858a22dd3cd2f5b11b095bdfe233c375194ce
Vyctor <dev.vyctor@gmail.com>
2021-10-09 20:40:14 -0300

    style: add space between functions declarations

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,9 +2,11 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (url: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL)
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
   },
