1bb596681ef87896ceea9a1ad76b039d995295b6
vyctorvguimaraes <vyctorguimaraes@conexia.com.br>
2022-04-09 23:12:12 -0300

    fix: fix return on LogControllerDecorator

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -12,8 +12,7 @@ class LogControllerDecorator implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.controller.handle(httpRequest)
-    return null
+    return await this.controller.handle(httpRequest)
   }
 }
 
