d8a605c4d9a88bd747ff03ddf2bc92e6d8bcce0f
Vyctor <dev.vyctor@gmail.com>
2022-07-24 15:11:45 -0300

    test: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -5,6 +5,7 @@ import { MissingParamError } from '../../errors/missing-param-error'
 
 const makeFakeHttpRequest = (): HttpRequest => ({
   body: {
+    email: 'any_email',
     password: 'any_password'
   }
 })
@@ -14,8 +15,20 @@ describe('Login Controller', () => {
     const sut = new LoginController()
 
     const httpRequest = makeFakeHttpRequest()
+    delete httpRequest.body.email
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new LoginController()
+
+    const httpRequest = makeFakeHttpRequest()
+
+    delete httpRequest.body.password
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 })
