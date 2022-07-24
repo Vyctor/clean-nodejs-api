de46ddd97a60b5be24d57bc1fe5d8e8315b08779
Vyctor <dev.vyctor@gmail.com>
2022-07-24 16:58:08 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -81,50 +81,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = makeFakeRequest()
-
-    delete httpRequest.body.name
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
-  })
-
-  test('Should return 400 if no email is provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = makeFakeRequest()
-
-    delete httpRequest.body.email
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-  })
-
-  test('Should return 400 if no password is provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = makeFakeRequest()
-
-    delete httpRequest.body.password
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-  })
-
-  test('Should return 400 if no password confirmation is provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = makeFakeRequest()
-
-    delete httpRequest.body.passwordConfirmation
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirmation')))
-  })
-
   test('Should return 400 if password confirmation is incorrect', async () => {
     const { sut } = makeSut()
 
